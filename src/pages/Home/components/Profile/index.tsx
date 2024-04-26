b2180a2250391515eb45b71af85c776c1939115e
Gabriel Santana <gabrielsantanasantos798@gmail.com>
2024-04-26 00:29:40 -0300

    Feat: Placing the image and name in the Profile component

--- src/pages/Home/components/Profile/index.tsx
+++ src/pages/Home/components/Profile/index.tsx
@@ -1,3 +1,11 @@
+import * as S from "./styles";
+import profile from "../../../../assets/profile.png";
+
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
+    <S.Container>
+      <S.Image src={profile} alt="Profile Image" />
+      <S.Name>Gabriel Santana</S.Name>
+    </S.Container>
+  );
 }
--- src/pages/Home/components/Profile/styles.ts
+++ src/pages/Home/components/Profile/styles.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components";
 
-export const Container = styled.main``;
+export const Container = styled.main`
+  display: flex;
+  align-items: center;
+`;
+
+export const Name = styled.span``;
+
+export const Image = styled.img`
+  width: 4rem;
+`;
